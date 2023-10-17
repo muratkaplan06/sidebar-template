c82119f508adaf32ecc139b504f47adb30eab11c
Murat Kaplan <muratkaplan061989@gmail.com>
2023-10-17 14:57:19 +0300

    sidebar

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+function Home() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <div>Home</div>
+      </Box>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Shorts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+function Shorts() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <div>Shorts</div>
+      </Box>
+    </>
+  )
+}
+
+export default Shorts
